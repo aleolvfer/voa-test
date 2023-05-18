a76237c2fdfe1e1f24b14d4ec5943274e5cf8c90
Alex Brendon <alex.brendon@voaremos.com.br>
2023-05-18 10:49:10 -0300

    sw - barcode teste

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -45,3 +45,14 @@ self.addEventListener('fetch', (event) => {
     })());
   }
 });
+
+const barcodeDetector = new BarcodeDetector({
+  formats: ["code_39", "codabar", "ean_13"],
+});
+
+// check compatibility
+if (barcodeDetector) {
+  console.log("Barcode Detector supported!");
+} else {
+  console.log("Barcode Detector is not supported by this browser.");
+}
