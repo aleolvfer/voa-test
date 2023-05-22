1a2e6e481a7fad5411b67b6f02e4d800844c6972
Alex Brendon <alex.brendon@voaremos.com.br>
2023-05-22 14:09:45 -0300

    push notification?

--- js/email-validation.js
+++ js/email-validation.js
@@ -1,18 +1,21 @@
 document.querySelector(".button").addEventListener("click", async event => {
   event.preventDefault();
 
+  var options = {
+    vibrate: [200, 100, 200],
+  };
+
   if (!("Notification" in window)) {
     alert("Este browser não suporta notificações de Desktop");
   }
-
   else if (Notification.permission === "granted") {
-    var notification = new Notification("Hi there!");
+    var notification = new Notification("Hi there!", options);
   }
 
   else if (Notification.permission !== 'denied') {
     Notification.requestPermission(function (permission) {
       if (permission === "granted") {
-        var notification = new Notification("Hi there!");
+        var notification = new Notification("Hi there!", options);
       }
     });
   }
